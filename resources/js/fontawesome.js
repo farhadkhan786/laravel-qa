06873d9479ffe5c1200986c922b1d72b92c2011e
farhadkhan786 <fhussainkhan579@gmail.com>
2021-10-04 14:46:15 +0500

    Add vote controls to question and answer items

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,7 @@
+import fontasweome from '@fortawesome/fontawesome-free';
+import {faCaretUp} from '@fortawesome/free-solid-svg-icons/faCaretUp';
+import {faCaretDown} from '@fortawesome/free-solid-svg-icons/faCaretDown';
+import {faStar} from '@fortawesome/free-solid-svg-icons/faStar';
+import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck';
+
+fontasweome.library.add([faCaretup, faCaretDown, faStar, faCheck])
