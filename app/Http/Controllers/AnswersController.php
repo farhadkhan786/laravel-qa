49217b0cc71777443b4ef73409b2b918b7085c43
farhadkhan786 <fhussainkhan579@gmail.com>
2021-10-05 08:36:34 +0500

    Implemented update answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -21,7 +21,7 @@ class AnswersController extends Controller
             'body'=> 'required'
         ]) +  ['body' => $request->body, 'user_id' => \Auth::id()]);
 
-        return back()->with('Success!', "Your answer has been submitted successfully");
+        return back()->with('success', "Your answer has been submitted successfully");
     }
 
     /**
@@ -30,9 +30,10 @@ class AnswersController extends Controller
      * @param  \App\Models\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+        return view('answers.edit', compact('question', 'answer' ));
     }
 
     /**
@@ -42,9 +43,14 @@ class AnswersController extends Controller
      * @param  \App\Models\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+        $answer->update($request->validate([
+            'body' => 'required',
+        ]));
+
+        return redirect()->route('questions.show', $question->slug)->with('success', "Your answer has been updated");
     }
 
     /**
