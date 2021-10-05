cd24a45eea34ff9e7f3c4f11e42e2b9e1cf5e43e
farhadkhan786 <fhussainkhan579@gmail.com>
2021-10-05 09:31:30 +0500

    Update best_answer_id on questions table when an answer removed using Eloquent event

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\Null_;
 
 class Answer extends Model
 {
@@ -31,11 +32,20 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if ($question->best_answer_id === $answer->id); {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
 
     public function getCreatedDateAttribute () {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute () {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
