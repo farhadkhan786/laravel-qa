f497a4c0ed48975aba83712c66d3819025a081a0
farhadkhan786 <fhussainkhan579@gmail.com>
2021-10-19 11:57:08 +0500

    Implement DRY principle in the Application views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -7,6 +7,7 @@
                 </div>
                 <hr>
                 @include('layouts._messeges')
+{{--                @include('shared._vote')--}}
                 @foreach ($answers as $answer)
                     <div class="media">
                         <div class="d-fex flex-column vote-controls">
@@ -34,23 +35,7 @@
                                 @csrf
                                 <input type="hidden" name="vote" value="-1">
                             </form>
-                            @can('accept', $answer)
-                                <a title="Mark this answer as best answer"
-                                    class="{{ $answer->status }} mt-2"
-                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();">
-                                    <i class="fas fa-check fa-2x"></i>
-                                </a>
-                                <form id="accept-answer-{{ $answer->id }}" action="{{ route('answers.accept', $answer->id) }}" method="post" style="display:none">
-                                    @csrf
-                                </form>
-                            @else
-                                @if($answer->is_best)
-                                    <a title="The question owner accepted this answer as best answer"
-                                       class="{{ $answer->status }} mt-2">
-                                        <i class="fas fa-check fa-2x"></i>
-                                    </a>
-                                @endif
-                            @endcan
+
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
@@ -72,17 +57,10 @@
                                 </div>
                                 <div class="col-4"></div>
                                 <div class="col-4">
-                                <span class="text-muted">
-                                Answered {{ $answer->created_date }}
-                                </span>
-                                    <div class="media mt-2">
-                                          <a href="{{ $answer->user->url }}" class="pr-2">
-                                               <img src="{{ $answer->user->avatar }}">
-                                          </a>
-                                       <div class="media-body mt-1">
-                                           <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                                       </div>
-                                    </div>
+                                    @include('shared._author', [
+                                        'model' => $answer,
+                                        'label' => 'answered'
+                                    ])
                                 </div>
                             </div>
                         </div>
