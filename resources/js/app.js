36665de7ae6be796a05889bcd2d06ace3cc8a40b
farhadkhan786 <fhussainkhan579@gmail.com>
2021-10-22 12:38:15 +0500

    Create accept answer component

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,7 +26,8 @@ Vue.use(VueIziToast);
 
 Vue.component('user-info', require('./components/UserInfo.vue').default);
 Vue.component('answer', require('./components/Answer.vue').default);
-Vue.component('favourite', require('./components/Favoruite.vue').default);
+Vue.component('favourite', require('./components/Favourite.vue').default);
+Vue.component('accept', require('./components/Accept.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/views/shared/_vote.blade.php
+++ resources/views/shared/_vote.blade.php
@@ -45,8 +45,6 @@
     @if ($model instanceof \App\Models\Question)
         <favourite :question="{{ $model }}"></favourite>
     @elseif ($model instanceof \App\Models\Answer)
-        @include('shared._accept', [
-            'model' => $model
-        ])
+        <accept :answer="{{ $model }}"></accept>
     @endif
 </div>
